9159f69020576036330c742013a0f40c26c16046
hotsmartphone <33004453+hotsmartphone@users.noreply.github.com>
2023-01-19 19:12:09 +0500

    WIP Обновляет клас ConfigService, добавляет доп. типы и значения в схему переменых окружения, устанавливает пакеты inversify и reflect-metadata, добавляет работу с контейнером

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import {Container} from 'inversify';
 import Application from './app/application.js';
+import {LoggerInterface} from './common/logger/logger.interface.js';
 import LoggerService from './common/logger/logger.service.js';
+import {ConfigInterface} from './common/config/config.interface.js';
 import ConfigService from './common/config/config.service.js';
+import {Component} from './types/component.types.js';
 
-const logger = new LoggerService();
-const config = new ConfigService(logger);
+const applicationContainer = new Container();
+applicationContainer.bind<Application>(Component.Application).to(Application);
+applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService);
+applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService);
 
-const application = new Application(logger, config);
+const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
