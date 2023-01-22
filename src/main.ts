10c49fd1134cad049286b7b7262a0f670ddc5077
hotsmartphone <33004453+hotsmartphone@users.noreply.github.com>
2023-01-22 15:46:09 +0500

    Добавляет контейнер с зависимостями. Внедряет зависимости с помощью декоратора. Inversify создает один экзепляр класса

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import {Container} from 'inversify';
 import Application from './app/application.js';
 import {LoggerInterface} from './common/logger/logger.interface.js';
@@ -7,9 +8,9 @@ import ConfigService from './common/config/config.service.js';
 import {Component} from './types/component.types.js';
 
 const applicationContainer = new Container();
-applicationContainer.bind<Application>(Component.Application).to(Application);
-applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService);
-applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService);
+applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
+applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
+applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
