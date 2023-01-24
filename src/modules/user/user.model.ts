d110033208bf2dc8da7a2de6b207167d0f8da36d
hotsmartphone <33004453+hotsmartphone@users.noreply.github.com>
2023-01-25 00:23:24 +0500

    WIP Добавляетвалидацию на User, добавляет модель, методы в модель, класс DTO

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -7,13 +7,30 @@ export interface UserDocument extends User, mongoose.Document {
 }
 
 const userSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    minLength: [1, 'Name is too short. Min length for name is 1'],
+    maxLength: [15, 'Name is too long. Max length for name is 15'],
+    required: true
+  },
   email: {
     type: String,
     unique: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+    required: true
   },
-  avatarPath: String,
-  password: String,
+  avatarPath: {
+    type: String,
+    minlength: [5, 'Min length for avatar path is 5'],
+    match: [/^\w*(.jpg|.png)$/, 'Format of avatar image is incorrect. Supported formats are ".jpg" and ".png"'],
+    required: false
+  },
+  password: {
+    type: String,
+    minLength: [6, 'Password is too short. Min length for password is 6'],
+    maxLength: [12, 'Password is too long. Max length for password is 12'],
+    required: true
+  },
   userType: String
 }, {
   timestamps: true
